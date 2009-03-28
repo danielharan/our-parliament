ce14d18a3ec956f21e0e1e959faf8cd43c7418b1
fsbrainstorm <fsbrainstorm@gmail.com>
2009-03-28 17:32:31 -0400

    More updates, now including MPs index page.

---
+++ test/functional/hansards_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class HansardsControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
---
+++ test/unit/helpers/hansards_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class HansardsHelperTest < ActionView::TestCase
+end
