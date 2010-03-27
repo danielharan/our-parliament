ba652f1318f4116187d4081ea570cb398f20b765
Daniel Haran <chebuctonian@mgmail.com>
2010-03-27 13:29:09 -0400

    spreadsheet view for MPs

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100326215536) do
+ActiveRecord::Schema.define(:version => 20100327171940) do
 
   create_table "mps", :force => true do |t|
     t.string  "ed_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20100326215536) do
     t.string  "facebook"
     t.string  "twitter"
     t.boolean "active",                   :default => true
+    t.string  "wikipedia_riding"
   end
 
   create_table "recorded_votes", :force => true do |t|
