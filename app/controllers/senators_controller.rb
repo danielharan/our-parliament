f16bc4e90c4c3532f1152e989cc809fc27ce7903
Daniel Haran <chebuctonian@mgmail.com>
2010-03-30 18:27:40 -0400

    senator edit

--- app/controllers/senators_controller.rb
+++ app/controllers/senators_controller.rb
@@ -1,10 +1,26 @@
 class SenatorsController < ApplicationController
+  before_filter :basic_admin, :only => [:edit, :update]
+  before_filter :find_senator, :except => [:index]
+  
   def index
     @senators = Senator.all
   end
   
   def show
     fetch_random_links
-    @senator = Senator.find params[:id]
   end
+  
+  def edit
+  end
+  
+  def update
+    @senator.update_attributes params[:senator]
+    
+    redirect_to senator_path(@senator)
+  end
+  
+  private
+    def find_senator
+      @senator = Senator.find params[:id]
+    end
 end
--- test/functional/senators_controller_test.rb
+++ test/functional/senators_controller_test.rb
@@ -24,4 +24,40 @@ class SenatorsControllerTest < ActionController::TestCase
     should_assign_to :senator, :article_links, :glossary_links
     should_respond_with :success
   end
+  
+  context "on GET to edit without auth" do
+    setup do
+      senator = Factory(:senator)
+      
+      get :edit, :id => senator.id
+    end
+    
+    should_respond_with 401
+  end
+
+  context "on GET to edit with auth" do
+    setup do
+      SenatorsController.any_instance.stubs(:basic_admin).returns(true)
+      senator = Factory(:senator)
+      
+      get :edit, :id => senator.id
+    end
+    
+    should_respond_with :success
+  end
+  
+  context "on PUT to update" do
+    setup do
+      SenatorsController.any_instance.stubs(:basic_admin).returns(true)
+      @senator = Factory(:senator, :name => "before")
+      
+      put :update, :id => @senator.id, :senator => {:name => "after"}
+    end
+    
+    should_respond_with :redirect
+    
+    should "have updated the senator's name" do
+      assert_equal "after", @senator.reload.name
+    end
+  end
 end
