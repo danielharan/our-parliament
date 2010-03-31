2df072e918cfccbca9f573b3a6501e428118f230
Daniel Haran <chebuctonian@mgmail.com>
2010-03-31 14:55:11 -0400

    can't get bill_number to display properly; not sure what to do

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -11,4 +11,8 @@ class Vote < ActiveRecord::Base
   def self.last(n)
     find(:all, :order => "vote_date DESC", :limit => n)
   end
+  
+  def total_votes
+    in_favour + opposed + paired
+  end
 end
--- test/unit/vote_test.rb
+++ test/unit/vote_test.rb
@@ -2,4 +2,10 @@ require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
   should_have_many :recorded_votes
+  
+  def test_total_votes
+    v = Factory(:vote, :in_favour => 100, :opposed => 10, :paired => 2)
+    
+    assert_equal 112, v.total_votes
+  end
 end
