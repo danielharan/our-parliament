fd93b37c6f21251750cb27b6a7ce78c09baa94c7
Shawn Simister <narphorium@gmail.com>
2010-05-31 08:49:10 -0400

    Fixed PostgreSQL syntax error

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -4,7 +4,7 @@ class Committee < ActiveRecord::Base
   #has_many :members, :class_name => "CommitteeMembership", :include => "mp"
   
   def current_members
-    return CommitteeMembership.find_by_sql(["SELECT m.* FROM committees c, committee_memberships m, mps mp WHERE c.id = ? AND c.id = m.committee_id AND m.mp_id = mp.id AND m.parliament = ? AND m.session = ? GROUP BY m.mp_id, mp.name ORDER BY mp.name", id, ENV['CURRENT_PARLIAMENT'].to_i, ENV['CURRENT_SESSION'].to_i])
+    return CommitteeMembership.find_by_sql(["SELECT m.* FROM committees c, committee_memberships m, mps mp WHERE c.id = ? AND c.id = m.committee_id AND m.mp_id = mp.id AND m.parliament = ? AND m.session = ? GROUP BY m.mp_id ORDER BY mp.name, m.mp_id", id, ENV['CURRENT_PARLIAMENT'].to_i, ENV['CURRENT_SESSION'].to_i])
   end
   
 end
