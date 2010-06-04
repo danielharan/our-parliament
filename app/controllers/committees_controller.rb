642ff0cc849de40b437412a7709fa43dba2cfbab
Shawn Simister <narphorium@gmail.com>
2010-06-04 10:52:20 -0400

    Trying to fix another Postgres bug.

--- app/controllers/committees_controller.rb
+++ app/controllers/committees_controller.rb
@@ -3,7 +3,7 @@ class CommitteesController < ApplicationController
   before_filter :fetch_random_links, :only => [:index, :show]
   
   def index
-    @current_committees = Committee.find_by_sql(["SELECT c.* FROM committees c, committee_memberships m WHERE c.id = m.committee_id AND m.parliament = ? AND m.session = ? GROUP BY c.id, c.name_#{I18n.locale} ORDER BY c.name_#{I18n.locale}", ENV['CURRENT_PARLIAMENT'].to_i, ENV['CURRENT_SESSION'].to_i])
+    @current_committees = Committee.find_by_sql(["SELECT c.*, Count(c.id) FROM committees c, committee_memberships m WHERE c.id = m.committee_id AND m.parliament = ? AND m.session = ? GROUP BY c.id ORDER BY c.name_#{I18n.locale}", ENV['CURRENT_PARLIAMENT'].to_i, ENV['CURRENT_SESSION'].to_i])
   end
 
   def show
